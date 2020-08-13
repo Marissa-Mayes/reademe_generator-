be469f332552f44ebf30f8bfc576e01566e02788
marissa-mayes <marissa.mayes1289@gmail.com>
2020-08-12 22:22:42 -0500

    created markdown and writetofile function

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ return inquirer.prompt([
     message: "What is the project description?",
     name: "description",
     },
-    console.log(userPrompt),
+    //console.log(userPrompt),
     {
     // type: "input",
     // message: "Provide Table of Contents",
@@ -51,7 +51,52 @@ return inquirer.prompt([
     message: "please enter email address",
     name: "email",
     },
-]);
-    };
+])
+}
+const stubs={
+    title: 'nick',
+    description: 'great coding',
+    install: 'no',
+    usage: 'extensively',
+    contribution: 'be nice',
+    license: 'mit',
+    github: 'nick',
+    email: 'nick@nick.nick'
+  }
+
+//TODO: generate markdown
+
+function generateMarkdown(data) {
+
+    for (key in data){
+        console.log(key, " is ",data[key])
+    }
+
+    return `# ${data.title}
+### Description: ${data.description}
+
+  `;
+  }
+  
+  //console.log(generateMarkdown(stubs))
+
+//TODO: wrtie to file
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,
+         data,
+          //{encoding: "utf-8"},
+          ()=>{console.log("you tried to wrtie a file")})
+}
+//writeToFile("nick2", "this is a story about nick")
+function init(){
+    userPrompt().then(answers=>{
+        console.log(answers)
+        const formattedString= generateMarkdown(answers)
+        writeToFile(answers.title, formattedString)
+    })
+}
+init()
+    
     
 
